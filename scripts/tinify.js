766254e289a81e3c3d146d40cc5ca636928d9fee
David May <David.May@efi.com>
2020-02-24 15:11:54 -0800

    [tinify] Directly downscale the main png to 300 pixels then tinify w/o intermediary file

---
+++ scripts/tinify.js
@@ -0,0 +1,14 @@
+const tinify = require('tinify')
+const ArgumentParser = require('argparse').ArgumentParser;
+const path = require('path')
+
+const parser = new ArgumentParser({ version: '0.0.1', addHelp:true, description: 'Tinify pngs (tinypng.com)'})
+parser.addArgument('apikey', { help: 'tinypng.com API key', })
+parser.addArgument('infile', { help: 'Path to png to be tinified', })
+const args = parser.parseArgs()
+args.outfile = `${path.dirname(args.infile)}${path.sep}${path.basename(args.infile, '.png')}.m.png`
+console.dir(args)
+
+tinify.key = args.apikey
+const resized = tinify.fromFile(args.infile).resize({method: 'scale', width: 300})
+resized.toFile(args.outfile)
