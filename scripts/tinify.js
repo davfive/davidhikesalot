0c688e3d7d8280748b06dd117016223591595e1e
David May <David.May@efi.com>
2020-02-24 15:14:43 -0800

    [tinify] Add direct url to API key page on tinypng.com to easily get my api key

--- scripts/tinify.js
+++ scripts/tinify.js
@@ -3,8 +3,8 @@ const ArgumentParser = require('argparse').ArgumentParser;
 const path = require('path')
 
 const parser = new ArgumentParser({ version: '0.0.1', addHelp:true, description: 'Tinify pngs (tinypng.com)'})
-parser.addArgument('apikey', { help: 'tinypng.com API key', })
-parser.addArgument('infile', { help: 'Path to png to be tinified', })
+parser.addArgument('apikey', { help: 'tinypng.com API key from https://tinypng.com/dashboard/api'})
+parser.addArgument('infile', { help: 'Path to png to be tinified'})
 const args = parser.parseArgs()
 args.outfile = `${path.dirname(args.infile)}${path.sep}${path.basename(args.infile, '.png')}.m.png`
 console.dir(args)
