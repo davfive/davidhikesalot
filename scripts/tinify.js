9dcc052fca4289d1c30f9ea9dddff4b1db743c35
David May <David.May@efi.com>
2020-02-24 16:17:11 -0800

    [tinify] Update tinify.js to create both a tinified web (1200xauto-height ~ 600kb w/ legible trail names) and tiny mobile (300xauto-height ~ 50kb). Clicking on this will give the full version

--- scripts/tinify.js
+++ scripts/tinify.js
@@ -6,9 +6,13 @@ const parser = new ArgumentParser({ version: '0.0.1', addHelp:true, description:
 parser.addArgument('apikey', { help: 'tinypng.com API key from https://tinypng.com/dashboard/api'})
 parser.addArgument('infile', { help: 'Path to png to be tinified'})
 const args = parser.parseArgs()
-args.outfile = `${path.dirname(args.infile)}${path.sep}${path.basename(args.infile, '.png')}.m.png`
+args.outwfile = `${path.dirname(args.infile)}${path.sep}${path.basename(args.infile, '.png')}.w.png`
+args.outmfile = `${path.dirname(args.infile)}${path.sep}${path.basename(args.infile, '.png')}.m.png`
 console.dir(args)
 
 tinify.key = args.apikey
-const resized = tinify.fromFile(args.infile).resize({method: 'scale', width: 300})
-resized.toFile(args.outfile)
+const wresized = tinify.fromFile(args.infile).resize({method: 'scale', width: 1200})
+wresized.toFile(args.outwfile)
+
+const mresized = tinify.fromFile(args.infile).resize({method: 'scale', width: 300})
+mresized.toFile(args.outmfile)
