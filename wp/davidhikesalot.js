8b4778d2e72e459d48e3d83962a33b1f1379e67f
David May <David.May@efi.com>
2020-07-08 14:08:59 -0700

    Hike stats section development

--- wp/davidhikesalot.js
+++ wp/davidhikesalot.js
@@ -174,7 +174,7 @@ const updateParkStats = (park, hikeStatus, distance, elevation) => {
 }
 
 const getStatsStringHtml = stats => {
-  return `${stats.hikes} hikes; ${stats..distance.toFixed(1).toLocaleString()} miles; ${elevation.toLocaleString()} elevation`
+  return `${stats.hikes} hikes; ${stats.distance.toFixed(1).toLocaleString()} miles; ${elevation.toLocaleString()} elevation`
 }
 
 const getStatsTableHtml = statsGroup => {
