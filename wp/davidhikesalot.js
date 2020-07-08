8570c8a6683bdec060e7256448f3e03de3fc8279
David May <David.May@efi.com>
2020-07-08 11:37:24 -0700

    Hike stats section development

--- wp/davidhikesalot.js
+++ wp/davidhikesalot.js
@@ -198,7 +198,7 @@ jQuery(document).ready(function($) {
 
     if (pageHasElement('#hikingStats')) {
       const statCols = ['planned', 'completed']
-      const years = Object.keys(OverallStats).filter(k => !isNaN(k))
+      const years = Object.keys(OverallStats).filter(k => !isNaN(k) && k >= 2019)
       statCols.push(...years.sort().reverse())
 
       let statTable = '<table class="stat-table"><thead><th></th>'
