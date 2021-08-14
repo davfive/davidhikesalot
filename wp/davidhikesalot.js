f20eef2fdca0df7708556669691f8143ca3ce4fe
David <davfive@gmail.com>
2021-08-14 15:56:02 -0700

    Fix cellDate lookup

--- wp/davidhikesalot.js
+++ wp/davidhikesalot.js
@@ -30,7 +30,7 @@ const cellIsEmpty = (row, id) => (['', '--'].indexOf(cellText(row, id)) >= 0)
 const strIsDate = s => Date.parse(s) !== NaN
 const strToDate = s => strIsDate(s) ? moment(s, 'DD/MMM/YYYY') : moment.invalid()
 const cellIsDate = (row, id) => strIsDate(cellText(row, id))
-const cellToDate = (row, id) => strToDate(cellIsDate(row, id))
+const cellToDate = (row, id) => strToDate(cellText(row, id))
 const getHikeDate = hikeRow => cellToDate(hikeRow, 'hikedate')
 
 const getHikeListByStatus = (hikeStatus, parkName) => {
